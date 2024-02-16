0f3867dddbbc4c89dc6ea87475576b5864058b20
radamanthiss <kevinsb-93@hotmail.com>
2024-02-15 22:24:28 -0500

    readme for project

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # beer_bar_api
+
+API BEER
+This project is a simple REST API built with Django and Django REST Framework (DRF) for managing beer orders it can be use for users to list available beers, create orders, and payments.
+
+## Prerequisites
+* Python
+* pip
+* virtualenv or conda management
+
+
+## Installation
+- clone the repo using this git clone https://github.com/radamanthiss/beer_bar_api.git
+- virtualenv venv
+- source venv/bin/activate or conda create -n beer_env python=3.9.18 if you have conda in your pc
+
+## install requirements
+- pip install -r requirements.txt
+
+## start the project
+- python manage.py migrate in this case is not necessary because we don't use database
+- python manage.py runserver
+- Your api is running at http://127.0.0.1:8000/
+
+# API usage
+## you can see the list of endpoints in the route http://127.0.0.1:8000/swagger/ or in next section each endpoint
+
+
+## List beers endpoint
+- endpoint GET /api/beers/
+- desc : get all the list of available beers
+
+## Receive an order endpoint
+- endpoint POST /api/order/
+- desc: create an order of beer
+- paylodad
+  {
+  "beer_id": 1,
+  "friend_name": "John Doe",
+  "quantity": 2
+  }
+
+
+## Payment a order or account endpoint
+- endpoint POST /api/pay/
+- desc: using for payments can be equal or individual payment
+
+### request for equal payment
+- {"payment_type": 'equal'}
+
+### request for individual payment
+- {
+    "payment_type":"individual",
+    "friend_name":"Kevin"
+  }
+
+## Account endpoint
+- endpoint GET /api/account/
+- desc: get the values for account 
+
+### request for all orders
+- /api/account/
+- response: {
+    "total_value": 30.0
+  }
+
+### request for specific name from order
+- /api/account/?friend_name=Kevin
+- response: {
+    "friend_name": "Kevin",
+    "total_value": 25.0
+  }
+
+# RECOMENDATIONS
+for a best performance to test the frontend, first call the endpoint for create and order to have data for test
--- beerbar/orders/views.py
+++ beerbar/orders/views.py
@@ -38,22 +38,36 @@ class OrderReceive(APIView):
 
 class GetAccounting(APIView):
   def get(self, request):
-    total_value = sum(beer.price * order.quantity for order in orders for beer in beers if order.beer_id == beer.id)
-    return Response({'total': total_value})
+    friend_name = request.query_params.get('friend_name')
+    if friend_name:
+      friend_orders = [order for order in orders if order.friend_name == friend_name]
+      total_value = sum(beer.price * order.quantity for order in friend_orders for beer in beers if order.beer_id == beer.id)
+      return Response({'friend_name': friend_name, 'total_value': total_value})
+    else:
+      total_value = sum(beer.price * order.quantity for order in orders for beer in beers if order.beer_id == beer.id)
+      return Response({'total_value': total_value})
 
 class PayOrder(APIView):
   def post(self, request):
-    payment_type = request.data.get('payment_type', 'equal')
-    if payment_type == 'equal':
-      total_value = sum(beer.price * order.quantity for order in orders for beer in beers if order.beer_id == beer.id)
-      value_per_friend = total_value / 3
-      return Response({'value_per_friend': value_per_friend})
-    elif payment_type == 'individual':
-      bill = {order.friend_name: 0 for order in orders}
-      for order in orders:
-        for beer in beers:
-          if order.beer_id == beer.id:
-            bill[order.friend_name] += order.quantity * beer.price
-      return Response(bill)
-    else:
+    payment_type = request.data.get('payment_type')
+    if payment_type not in ['equal', 'individual']:
       return Response({'message': 'Invalid payment type'}, status=400)
+    if payment_type == 'equal':
+      if not orders:
+        return Response({'message': 'No orders to pay'}, status=400)
+      total_value = sum(beer.price * order.quantity for order in orders for beer in beers if order.beer_id == beer.id)
+      if total_value == 0:
+        return Response({'message': 'No orders found'}, status=400)
+      value_friend = total_value / 3
+      return Response({'message': f'Each friend should pay {value_friend:.2f}'})
+    
+    elif payment_type == 'individual':
+      friend_name = request.data.get('friend_name')
+      if not friend_name:
+        return Response({'message': 'Friend name is required when individual payment type is choosed'}, status=400)
+      individual_friend_orders = [order for order in orders if order.friend_name == friend_name]
+      if not individual_friend_orders:
+        return Response({'message': f'No orders found for {friend_name}'}, status=400)
+      value_friend = sum(beer.price * order.quantity for order in individual_friend_orders for beer in beers if order.beer_id == beer.id)
+      return Response({'message': f'{friend_name} should pay {value_friend:.2f}'})
+    return Response({'message': 'Invalid request'}, status=400)
